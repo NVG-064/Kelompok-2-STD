cb4aeccfd5e262051e8e79835926046a16a17c2f
NVG-64 <aricandra064@student.esaunggul.ac.id>
2021-07-25 23:52:23 +0700

    Redesign code to make it readable

--- src/tugas2.cpp
+++ src/tugas2.cpp
@@ -2,11 +2,10 @@
 using namespace std;
 
 #define N 7
+#define M 6 // (maximum_vertex_value) - 1
 #define UND INT_MAX
 
-#define M 6 // (maximum_vertex_value) - 1
-
-// history count
+// To save the data of cost after find the max value
 int dataHistory[M] = {};
 
 // Returns longest distance from 0 to N-1.
@@ -18,22 +17,22 @@ int longestDistaceByForwardMethod(int graph[N][N]) {
 	cost[N-1] = 0;
 
 	// Calculating longest path for rest of the nodes
-	for (int i = N-2 ; i >= 0 ; i--)
-	{
+	for (int i = N-2; i >= 0; i--) {
 
 		// Initialize distance from i to destination (N-1)
 		cost[i] = 0;
 
 		// Check all nodes of next stages to find longest distance from i to N-1.
-		for (int j = i ; j < N ; j++)
-		{
+		for (int j = i; j < N; j++) {
 			// Reject if no edge exists
 			if (graph[i][j] == UND)
 				continue;
 
-			// We apply recursive equation to distance to target through j. and compare with minimum distance so far.
+			// We apply recursive equation to distance to target through j and compare with maximum distance so far.
 			cost[i] = max(cost[i], graph[i][j] + cost[j]);
-            dataHistory[i] = cost[i]; // To save the history
+
+			// Everytime max value of cost is found, it will be saved to array with integer datatype.
+            dataHistory[i] = cost[i];
 		}
 	}
 
